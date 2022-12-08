d4f8b37bfb5853913bc68fc12b69d494d574c24f
Mateusz <56720382+MateuszPapiez@users.noreply.github.com>
2022-12-08 19:36:05 +0100

    add project setup

---
+++ Countries_application/Countries_application/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Countries_application
+//
+//  Created by Mateusz Papież on 08/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Countries_application/Countries_application/Countries_applicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Countries_applicationApp.swift
+//  Countries_application
+//
+//  Created by Mateusz Papież on 08/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Countries_applicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
