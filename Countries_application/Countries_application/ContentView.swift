7fb1d857c3f75f4dca4aecbf386dcd662e1b1a8e
MateuszPapiez <papiezmateusz@gmail.com>
2022-12-08 20:39:18 +0100

    fix comments

--- Countries_application/Countries_application/ContentView.swift
+++ Countries_application/Countries_application/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        NavigationView{
+        NavigationView {
             
             VStack {
                 Text("Todo : country list")
